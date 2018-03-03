7f5b7b2cf328f61bfbc98268279fbe5d001add81
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-03 10:03:37 -0600

    Lint HasOptions

--- src/lib/HasOptions.ts
+++ src/lib/HasOptions.ts
@@ -1,12 +1,12 @@
-import { ClassDecorator, Options } from "./interfaces";
+import { ClassDecorator, IOptions } from "./interfaces";
 
 export function HasOptions(): ClassDecorator {
     return function classDecorator<T extends { new(...args: any[]): {} }>(constructor: T): T {
         return class extends constructor {
-            public options: Options = { defaults: {} };
+            public options: IOptions = { defaults: {} };
             constructor(...args: any[]) {
                 super(...args);
             }
-        }
-    }
+        };
+    };
 }
