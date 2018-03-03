fafc5ad592b20ad942f134d6ce242075594908ee
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-03 09:48:37 -0600

    Create an Options object

--- src/lib/HasOptions.ts
+++ src/lib/HasOptions.ts
@@ -1,9 +1,9 @@
-import { ClassDecorator } from "./interfaces";
+import { ClassDecorator, Options } from "./interfaces";
 
 export function HasOptions(): ClassDecorator {
     return function classDecorator<T extends { new(...args: any[]): {} }>(constructor: T): T {
         return class extends constructor {
-            public options: any = {};
+            public options: Options = { defaults: {} };
             constructor(...args: any[]) {
                 super(...args);
             }
