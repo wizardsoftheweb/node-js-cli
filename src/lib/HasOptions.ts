7d493e7e041ae655fd1ad96605cc70a6e216fe0e
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-03 10:28:08 -0600

    Assign only when DNE

--- src/lib/HasOptions.ts
+++ src/lib/HasOptions.ts
@@ -3,9 +3,10 @@ import { ClassDecorator, IOptions } from "./interfaces";
 export function HasOptions(): ClassDecorator {
     return function classDecorator<T extends { new(...args: any[]): {} }>(constructor: T): T {
         return class extends constructor {
-            public options: IOptions = { defaults: {} };
+            public options: IOptions;
             constructor(...args: any[]) {
                 super(...args);
+                this.options = this.options || { defaults: {} };
             }
         };
     };
