d39a27f6a49fa4a61c94b1b267d7b1261d196363
CJ Harries <cj@wizardsoftheweb.pro>
2018-03-03 10:57:06 -0600

    Simplify test and clean copypasta

--- test/lib/HasOptions.spec.ts
+++ test/lib/HasOptions.spec.ts
@@ -14,18 +14,16 @@ import { HasOptions } from "../../src/lib/HasOptions";
 // This is a decorator, so we have to test lots of classes.
 /* tslint:disable:max-classes-per-file */
 describe("HasOptions", (): void => {
-    describe("The factory should expose winston", (): void => {
-        it("The factory should expose winston", (): void => {
-            @HasOptions()
-            class DecoratedClass {
-                constructor() {
-                    // do nothing
-                }
+    it("The factory should expose winston", (): void => {
+        @HasOptions()
+        class DecoratedClass {
+            constructor() {
+                // do nothing
             }
-            const decorated: any = new DecoratedClass();
-            decorated.should.have.property("options");
-            decorated.options.should.have.property("defaults");
-        });
+        }
+        const decorated: any = new DecoratedClass();
+        decorated.should.have.property("options");
+        decorated.options.should.have.property("defaults");
     });
 });
 /* tslint:enable:max-classes-per-file */
